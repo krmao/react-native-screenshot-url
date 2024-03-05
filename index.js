35496ab686b5f163bdc7a5aa5c95ade65bf0ebc7
kr.mao <kr.mao@qq.com>
2024-03-05 14:24:51 +0800

    feat: 0.0.7

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // noinspection NpmUsedModulesInstalled,JSUnresolvedReference
 
-const { NativeModules, NativeEventEmitter, DeviceEventEmitter, Platform , PermissionsAndroid} = require('react-native');
+const {NativeModules, NativeEventEmitter, DeviceEventEmitter, Platform, PermissionsAndroid} = require('react-native');
 
 let screenShotEmitter = undefined
 
@@ -10,114 +10,110 @@ let screenShotEmitter = undefined
  * android 默认不包含状态时间
  * ios 截屏不包含时间
  */
-export default class ScreenShotUtil  {
-   static checkPermissionsForScreenShot = async () => {
-    if (Platform.OS === 'ios') {
-      return Promise.resolve(true);
-    }
-    const SDK_INT = +Platform.constants.Release;
-    if (SDK_INT > 22) {
-      if (SDK_INT > 32) {
-        return (
-            (await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES)) &&
-            (await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VEDIO))
-        );
-      } else {
-        return await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
-      }
-    } else {
-      return Promise.resolve(true);
-    }
-  };
+export default class ScreenShotUtil {
+    static checkPermissionsForScreenShot = async () => {
+        if (Platform.OS === 'ios') {
+            return Promise.resolve(true);
+        }
+        const SDK_INT = +Platform.constants.Release;
+        if (SDK_INT > 22) {
+            return await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE) &&
+                (await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES)) &&
+                (await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VEDIO));
+        } else {
+            return Promise.resolve(true);
+        }
+    };
 
-  static requestPermissionsForScreenShot = async () => {
-    if (Platform.OS === 'ios') {
-      return Promise.resolve(null);
+    static requestPermissionsForScreenShot = async () => {
+        if (Platform.OS === 'ios') {
+            console.log("---- requestPermissionsForScreenShot null, ios")
+            return Promise.resolve(null);
+        }
+        const SDK_INT = +Platform.constants.Release;
+        if (SDK_INT > 22) {
+            return await PermissionsAndroid.requestMultiple([PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+                PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+                PermissionsAndroid.PERMISSIONS.READ_MEDIA_VEDIO
+            ]);
+        } else {
+            console.log("---- requestPermissionsForScreenShot null, SDK_INT <= 22")
+            return Promise.resolve(null);
+        }
+    };
+
+    /**
+     * 开始监听截屏事件， keyWords android 监听数据变化，截屏文件名的关键字
+     * @param {*} callBack
+     * @param keyWords
+     */
+    static async startListener(callBack, keyWords) {
+        if (!(await ScreenShotUtil.checkPermissionsForScreenShot())) {
+            await ScreenShotUtil.requestPermissionsForScreenShot();
+        }
+        if (await ScreenShotUtil.checkPermissionsForScreenShot()) {
+            return ScreenShotUtil.startListenerWithoutPermission(callBack, keyWords);
+        } else {
+            console.log("---- checkPermissionsForScreenShot false")
+            return Promise.resolve(null);
+        }
     }
 
-    const SDK_INT = +Platform.constants.Release;
-    if (SDK_INT > 22) {
-      if (SDK_INT > 32) {
-        return await PermissionsAndroid.requestMultiple([
-          PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
-          PermissionsAndroid.PERMISSIONS.READ_MEDIA_VEDIO,
-        ]);
-      } else {
-        return await PermissionsAndroid.requestMultiple([PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE]);
-      }
-    } else {
-      return Promise.resolve(null);
+    static startListenerWithoutPermission(callBack, keyWords) {
+        console.log("---- startListenerWithoutPermission")
+        const ScreenShot = NativeModules.ScreenShot;
+        // 创建自定义事件接口
+        screenShotEmitter && screenShotEmitter.removeAllListeners('ScreenShot')
+        screenShotEmitter = Platform.OS === 'ios' ? new NativeEventEmitter(ScreenShot) : DeviceEventEmitter;
+        screenShotEmitter.addListener('ScreenShot', (data) => {
+            console.log("---- startListenerWithoutPermission callBack", data)
+            if (callBack) {
+                callBack(data)
+            }
+        })
+        if (Platform.OS === 'android') {
+            console.log("---- startListenerWithoutPermission startListener")
+            ScreenShot.startListener(keyWords);
+        } else {
+            ScreenShot.startListener();
+        }
+        return screenShotEmitter
     }
-  };
 
-  /**
-   * 开始监听截屏事件， keyWords android 监听数据变化，截屏文件名的关键字
-   * @param {*} callBack
-   * @param keyWords
-   */
-  static async startListener(callBack, keyWords) {
-    if (!(await ScreenShotUtil.checkPermissionsForScreenShot())) {
-      await ScreenShotUtil.requestPermissionsForScreenShot();
+    /**
+     * 停止监听
+     */
+    static stopListener() {
+        screenShotEmitter && screenShotEmitter.removeAllListeners('ScreenShot')
+        const ScreenShot = NativeModules.ScreenShot;
+        return ScreenShot.stopListener();
     }
-    if (await ScreenShotUtil.checkPermissionsForScreenShot()) {
-      return ScreenShotUtil.startListenerWithoutPermission(callBack, keyWords);
-    }else{
-      return Promise.resolve(null);
+
+    /**
+     * 清除截屏缓存文件
+     * @param {*} callBack
+     */
+    static clearCache(callBack) {
+        const ScreenShot = NativeModules.ScreenShot;
+        ScreenShot.clearCache().then(res => {
+            callBack && callBack(res)
+        }).catch(err => {
+            callBack && callBack(err)
+        })
     }
-  }
 
-  static startListenerWithoutPermission (callBack, keyWords) {
-    const ScreenShot = NativeModules.ScreenShot;
-    // 创建自定义事件接口
-    screenShotEmitter && screenShotEmitter.removeAllListeners('ScreenShot')
-    screenShotEmitter = Platform.OS === 'ios' ? new NativeEventEmitter(ScreenShot) : DeviceEventEmitter;
-    screenShotEmitter.addListener('ScreenShot', (data) => {
-      if (callBack) {
-        callBack(data)
-      }
-    })
-    if (Platform.OS === 'android') {
-      ScreenShot.startListener(keyWords);
-    } else {
-      ScreenShot.startListener();
+    /**
+     * 截取当前屏幕方法
+     */
+    static screenShot = (callBack, isHiddenStatus, {extension = 'png', quality = 100, scale = 0}) => {
+        const ScreenShot = NativeModules.ScreenShot;
+        if (isHiddenStatus === undefined || isHiddenStatus === null) {
+            isHiddenStatus = Platform.OS === 'android'
+        }
+        ScreenShot.screenShot(isHiddenStatus, extension || 'png', quality || 100, scale || 0).then(res => {
+            callBack && callBack(res)
+        }).catch(err => {
+            callBack && callBack(err)
+        })
     }
-    return screenShotEmitter
-  }
-
-  /**
-   * 停止监听
-   */
-  static stopListener () {
-    screenShotEmitter && screenShotEmitter.removeAllListeners('ScreenShot')
-    const ScreenShot = NativeModules.ScreenShot;
-    return ScreenShot.stopListener();
-  }
-
-  /**
-   * 清除截屏缓存文件
-   * @param {*} callBack
-   */
-  static clearCache (callBack) {
-    const ScreenShot = NativeModules.ScreenShot;
-    ScreenShot.clearCache().then(res=>{
-      callBack && callBack(res)
-    }).catch(err=>{
-      callBack && callBack(err)
-    })
-  }
-
-  /**
-   * 截取当前屏幕方法
-   */
-  static screenShot = (callBack, isHiddenStatus, { extension = 'png', quality = 100, scale = 0 }) => {
-    const ScreenShot = NativeModules.ScreenShot;
-    if (isHiddenStatus === undefined || isHiddenStatus === null) {
-      isHiddenStatus = Platform.OS === 'android'
-    }
-    ScreenShot.screenShot(isHiddenStatus, extension || 'png', quality || 100, scale || 0).then(res=>{
-      callBack && callBack(res)
-    }).catch(err=>{
-      callBack && callBack(err)
-    })
-  }
 }
